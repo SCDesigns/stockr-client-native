13a366e912c86c5e0b0cdf65394784d8d8019937
Sean Clarke <xseanclarke@gmail.com>
2017-12-11 10:41:40 -0500

    Added src/ directory and started Header / index files

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Stockr: A Crypto App by Sean Clarke</Text>
+    </View>
+  )
+}
+  export default Header;
