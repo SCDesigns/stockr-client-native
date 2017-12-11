63f7365cbb89ff337374a4395070b358f6ffe846
Sean Clarke <xseanclarke@gmail.com>
2017-12-11 13:09:32 -0500

    defined Reducer +  Actions for Fetch/Success/Failure coinData

--- src/Reducers/StockrReducer.js
+++ src/Reducers/StockrReducer.js
@@ -1,3 +1,42 @@
+const initialState = {
+  isFetching: false,
+  data: null,
+  hasError: false,
+  errorMessage: null
+}
+
+export default function (state = initialState, action){
+  return state
+}
+
 export default function(state = [], action) {
-    return state
+  switch (action.type) {
+
+    case FETCHING_COIN_DATA:
+      return (...state, {
+        isFetching: true,
+        data: null,
+        hasError: false,
+        errorMessage: null
+      });
+
+    case FETCHING_COIN_DATA_SUCCESS:
+      return (...state, {
+        isFetching: false,
+        data: action.payload,
+        hasError: false,
+        errorMessage: null
+      });
+
+    case FETCHING_COIN_DATA_FAILURE:
+      return (...state, {
+        isFetching: false,
+        data: null,
+        hasError: true,
+        errorMessage: action.payload
+      });
+
+    default:
+      return state;
+  }
 }
