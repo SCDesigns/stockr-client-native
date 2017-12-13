4997da843f6e583b90923daba78d20d5b6ad367e
Sean Clarke <xseanclarke@gmail.com>
2017-12-12 19:25:44 -0500

    created Card for each coin

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Card = (props) => {
+  return (
+    <View>
+      <Text>props.symbol</Text>
+      <Text>props.price_usd</Text>
+      <Text>props.symbol</Text>
+    </View>
+
+  )
+};
+
+export default Card;
