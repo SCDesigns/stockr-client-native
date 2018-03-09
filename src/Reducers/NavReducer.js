b6fa949619e823bd4921b1b530353dfa41146441
Sean Clarke <xseanclarke@gmail.com>
2018-03-09 12:01:30 -0500

    feat: added reducers for nav and plugged into root

---
+++ src/Reducers/NavReducer.js
@@ -0,0 +1,4 @@
+const navReducer = (state, action) => {
+  const newState = AppNavigation.router.getStateForAction(action, state)
+  return newState || state
+}
--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import CryptoReducer from './CryptoReducer';
+import AppNavigation from '../Navigation/AppNavigation'
 
 export default combineReducers({
-    crypto: CryptoReducer
+    crypto: CryptoReducer,
+    nav: navReducer
 });
