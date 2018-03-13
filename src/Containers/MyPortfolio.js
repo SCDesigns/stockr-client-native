302e94fbeb10d96fdcfa5b3c391dfb8a5b04b0af
Sean Clarke <xseanclarke@gmail.com>
2018-03-13 14:27:51 -0400

    feat: added generic boilerplate page for MyPortfolio

--- src/Containers/MyPortfolio.js
+++ src/Containers/MyPortfolio.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 
-export default class Screen1 extends React.Component {
+export default class MyPortfolio extends React.Component {
   static navigationOptions = {
-    drawerLabel: 'Screen One',
+    drawerLabel: 'my Portfolio',
     drawerIcon: () => (
       <Image
-        source={{uri: `https://dummyimage.com/60x60/000/fff.jpg&text=BrowseCoins`}}
+        source={{uri: `https://dummyimage.com/60x60/000/fff.jpg&text=My Portfolio`}}
         style={{width: 30, height: 30, borderRadius: 15}}
       />
     )
